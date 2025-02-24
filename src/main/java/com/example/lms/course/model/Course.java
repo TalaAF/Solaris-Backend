65fe615acc7ed7b8c73cefe5f5816553dd5af5a9
SajaShawawra <202202357@bethlehem.edu>
2025-02-24 18:09:55 +0200

    feat(course): add Course entity

--- src/main/java/com/example/lms/course/model/Course.java
+++ src/main/java/com/example/lms/course/model/Course.java
@@ -1,5 +1,39 @@
 package com.example.lms.course.model;
 
+import com.example.lms.user.model.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "courses")
 public class Course {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "instructor_id", nullable = false)
+    private User instructor;
+
+    @ManyToMany
+    @JoinTable(
+        name = "course_students",
+        joinColumns = @JoinColumn(name = "course_id"),
+        inverseJoinColumns = @JoinColumn(name = "student_id")
+    )
+    private Set<User> students = new HashSet<>();
 }
