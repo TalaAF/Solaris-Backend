45599a579b8c1353962fdf487c9b4d9a330e7a77
SajaShawawra <202202357@bethlehem.edu>
2025-02-24 18:11:38 +0200

    feat(course): add CourseRepository interface

--- src/main/java/com/example/lms/course/repository/CourseRepository.java
+++ src/main/java/com/example/lms/course/repository/CourseRepository.java
@@ -1,5 +1,10 @@
 package com.example.lms.course.repository;
 
-public class CourseRepository {
-    
+import com.example.lms.course.model.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+    boolean existsByName(String name);
 }
