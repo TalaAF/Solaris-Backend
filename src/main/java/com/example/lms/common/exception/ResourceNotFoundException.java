8f2034b31b30b9b890ab3c96a96a033537c5992c
TalaAF <202202328@bethlehm.edu>
2025-03-06 17:11:28 +0200

    feat: Create Permission entity

--- src/main/java/com/example/lms/common/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.lms.common.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/example/lms/user/model/Role.java
+++
@@ -1,9 +0,0 @@
-// src/main/java/com/example/lms/user/model/Role.java
-package com.example.lms.user.model;
-
-// This enum defines the possible roles a user can have in our system
-public enum Role {
-    ADMIN,          // Can manage everything in the system
-    INSTRUCTOR,     // Can create and manage courses
-    STUDENT         // Can enroll in and access courses
-}
