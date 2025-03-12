7f3a0c35cad83fb45b6e0104e5fa4c9cebf6f1e0
SajaShawawra <202202357@bethlehem.edu>
2025-03-12 19:56:45 +0200

    feat(user-logging): integrate activity logging for course completion

--- src/main/java/com/example/lms/notification/service/CompletionNotificationService.java
+++ src/main/java/com/example/lms/notification/service/CompletionNotificationService.java
@@ -4,6 +4,8 @@ import com.example.lms.notification.model.CompletionNotification;
 import com.example.lms.notification.repository.CompletionNotificationRepository;
 import com.example.lms.user.model.User;
 import com.example.lms.course.model.Course;
+import com.example.lms.logging.service.UserActivityLogService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
@@ -15,6 +17,8 @@ public class CompletionNotificationService {
     
     @Autowired
     private EmailService emailService;
+    @Autowired
+private UserActivityLogService logService;
     
     public void sendCompletionNotification(User student, Course course, String message) {
         CompletionNotification notification = new CompletionNotification();
@@ -24,7 +28,7 @@ public class CompletionNotificationService {
         
         // Save notification using repository instance
         notificationRepository.save(notification); 
-    
+        logService.logActivity(student, "COURSE_COMPLETION", "Completed course: " + course.getName());
         // Send email notification
         String subject = "Course Completion Notification";
         String emailMessage = "Dear " + student.getFullName() + ",\n\n" + message;
