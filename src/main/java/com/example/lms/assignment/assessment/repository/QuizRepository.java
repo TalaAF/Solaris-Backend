ac622fa83e344f772edf87dd894ed6108c91ec88
Maysam-Hammad <202202212@bethlehem.edu>
2025-03-21 10:47:43 +0200

    refactor(repository): remove QuizRepository interface as part of restructuring

--- src/main/java/com/example/lms/assignment/assessment/repository/QuizRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.lms.assignment.assessment.repository;
-
-import com.example.lms.assignment.assessment.model.Quiz;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuizRepository extends JpaRepository<Quiz, Long> {
-}
